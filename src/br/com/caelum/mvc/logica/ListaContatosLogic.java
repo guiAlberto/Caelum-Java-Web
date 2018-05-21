07715efa56f4443df83de0a459fabf3c0b9c8d1a
Guilherme Alberto de Moraes <g.alberto.moraes@gmail.com>
2018-05-20 21:42:42 -0300

    Item 9.10
    Escondendo nossas páginas

--- src/br/com/caelum/mvc/logica/ListaContatosLogic.java
+++ src/br/com/caelum/mvc/logica/ListaContatosLogic.java
@@ -14,7 +14,7 @@ public class ListaContatosLogic implements Logica {
 	public String executa(HttpServletRequest req, HttpServletResponse res) {
 		List<Contato> contatos = new ContatoDao().getLista();
 		req.setAttribute("contatos", contatos);
-		return "lista-contatos.jsp";
+		return "/WEB-INF/jsp/lista-contatos.jsp";
 	}
 
 }
